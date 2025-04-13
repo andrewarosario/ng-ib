9600b4ae7a35e041417ba28fd88f02aacffbe830
andrewarosario <andrew.arosario@gmail.com>
2025-04-13 09:32:48 -0300

    feat: add internet banking pages

---
+++ src/app/pages/bank-statement/bank-statement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-bank-statement',
+  imports: [RouterModule],
+  templateUrl: './bank-statement.component.html',
+  styleUrl: './bank-statement.component.scss',
+})
+export class BankStatementComponent {}
